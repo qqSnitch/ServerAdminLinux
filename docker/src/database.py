faabbe51601e6ba8713dd3845dc37c330d6b6394
qqSnitch <128982937+qqSnitch@users.noreply.github.com>
2025-05-17 10:15:03 +0300

    Update database.py

--- docker/src/database.py
+++ docker/src/database.py
@@ -2,7 +2,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-engine = create_async_engine('postgresql+psycopg://kubsu:kubsu@127.0.0.1:5432/kubsu', echo=True)
+db =os.getenv("DATABASE_URL")
+engine = create_async_engine(db, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
