aa53fae57191053957196c3454f26906c5363c03
qqSnitch <128982937+qqSnitch@users.noreply.github.com>
2025-05-23 15:27:06 +0300

    Update database.py

--- docker/src/database.py
+++ docker/src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_async_engine('postgresql+psycopg://kubsu:kubsu@127.0.0.1:8099/kubsu', echo=True)
+engine = create_async_engine('postgresql+psycopg://kubsu:kubsu@127.0.0.1:5432/kubsu', echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
